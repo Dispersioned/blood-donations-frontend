d9a4da23a06d023c6b0ad49efa8a2721331ed2b5
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-07 23:18:52 +0300

    Update model.ts

--- src/entities/viewer/model.ts
+++ src/entities/viewer/model.ts
@@ -21,12 +21,10 @@ const exitFx = createEffect(() => {
   localStorage.removeItem('token');
 });
 
-export const $authPending = createStore(true);
 const $token = createStore(localStorage.getItem('token'));
+export const $authPending = createStore(true);
 export const $user = createStore<IUser | null>(null);
 $user.on(exit, () => null);
-
-$authPending.watch((v) => console.log('pending', v));
 
 $authPending.on(loginFx.pending, (_, is) => is);
 $authPending.on(validateTokenFx.pending, (_, is) => is);
