216ca06bc53b2ecd97582b2ebb16b284bea6d3ca
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-07 19:09:52 +0300

    ref: routing config

--- src/app/router/Routing.tsx
+++ src/app/router/Routing.tsx
@@ -1,13 +1,14 @@
 import { Auth } from 'pages/auth';
 import { Home } from 'pages/home';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { ROUTES } from 'shared/config/routes';
 
 export function Routing() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth/login" element={<Auth type="login" />} />
-      <Route path="/auth/register" element={<Auth type="register" />} />
+      <Route index element={<Home />} />
+      <Route path={ROUTES.login} element={<Auth type="login" />} />
+      <Route path={ROUTES.register} element={<Auth type="register" />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
