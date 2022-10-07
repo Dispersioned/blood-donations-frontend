fb5a1cb081bf860b1086d2306ff7c6d9f809165a
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-07 23:38:08 +0300

    feat: disable timestamps for roles and blood
    Revert "feat: disable timestamps for roles and blood"
    
    This reverts commit 9d4e8eda20be1099d823e399d8c49402090db4fe.
    
    Update types.ts

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -5,13 +5,9 @@ export type IUser = {
   username: string;
   roleId: number;
   bloodId: number;
-  createdAt: Date;
-  updatedAt: Date;
   role: {
     id: number;
     value: IRoleName;
-    createdAt: Date;
-    updatedAt: Date;
   };
 };
 
