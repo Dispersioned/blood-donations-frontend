54010f2d176d2f56782324fba13baeac8b8a6685
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-07 19:09:37 +0300

    ref: rename interface

--- src/pages/auth/model.ts
+++ src/pages/auth/model.ts
@@ -2,9 +2,9 @@ import { createEvent, split } from 'effector';
 import { login, register } from 'entities/viewer';
 import { AuthDto } from 'shared/types';
 
-type IPayload = { type: 'login' | 'register'; data: AuthDto };
+export type IAuthPayload = { type: 'login' | 'register'; data: AuthDto };
 
-export const auth = createEvent<IPayload>();
+export const auth = createEvent<IAuthPayload>();
 
 split({
   source: auth,
@@ -13,7 +13,7 @@ split({
     register: ({ type }) => type === 'register',
   },
   cases: {
-    login: login.prepend<IPayload>((payload) => payload.data),
-    register: register.prepend<IPayload>((payload) => payload.data),
+    login: login.prepend<IAuthPayload>((payload) => payload.data),
+    register: register.prepend<IAuthPayload>((payload) => payload.data),
   },
 });
