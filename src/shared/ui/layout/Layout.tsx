902c680af4892967268391d9d7cb5ffaadafd55f
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-07 20:56:44 +0300

    feat: create nav widget

--- src/shared/ui/layout/Layout.tsx
+++ src/shared/ui/layout/Layout.tsx
@@ -1,12 +1,21 @@
-import { Container } from '@mui/material';
+import { Container, styled } from '@mui/material';
 import React from 'react';
+
+import { Aside } from './Aside';
 
 type LayoutProps = React.PropsWithChildren;
 
+export const Wrapper = styled('div')`
+  display: grid;
+  grid-template-columns: 250px 1fr;
+  min-height: 100vh;
+`;
+
 export function Layout({ children }: LayoutProps) {
   return (
-    <Container maxWidth="xl" style={{ minHeight: '100vh' }}>
-      {children}
-    </Container>
+    <Wrapper>
+      <Aside />
+      <Container maxWidth="xl">{children}</Container>
+    </Wrapper>
   );
 }
