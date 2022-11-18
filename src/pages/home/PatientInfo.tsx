99886a6b0f9e140c671c44eb34ad62c25e9a3118
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-11-18 14:15:31 +0300

    feat: render personal patient info

--- src/pages/home/PatientInfo.tsx
+++ src/pages/home/PatientInfo.tsx
@@ -1,3 +1,5 @@
+import { Typography, styled } from '@mui/material';
+import { useUnit } from 'effector-react';
 import React, { useEffect } from 'react';
 
 import { patientInfoModel } from '.';
@@ -6,10 +8,35 @@ type PatientInfoProps = {
   patientId: number;
 };
 
+const Wrapper = styled('div')`
+  margin-top: 10px;
+`;
+
+const Flex = styled('div')`
+  display: flex;
+  gap: 15px;
+  align-items: center;
+`;
+
 export function PatientInfo({ patientId }: PatientInfoProps) {
   useEffect(() => {
     patientInfoModel.fetch({ patientId });
   }, [patientId]);
 
-  return <div>PatientInfo</div>;
+  const patientInfo = useUnit(patientInfoModel.$patientInfo);
+
+  if (!patientInfo) return null;
+
+  return (
+    <Wrapper>
+      <Flex>
+        <Typography variant="h5">Лечащий врач:</Typography>
+        <Typography variant="h6">{patientInfo.doctor.username}</Typography>
+      </Flex>
+      <Flex>
+        <Typography variant="h5">Больница:</Typography>
+        <Typography variant="h6">{patientInfo.hospital.name}</Typography>
+      </Flex>
+    </Wrapper>
+  );
 }
